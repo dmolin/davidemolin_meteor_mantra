1a6de9bb9c90496d4ad909ebf478e4b9ce2dd524
dmolin <davide.molin@gmail.com>
2016-03-25 23:52:25 +0000

    completing testimonials

--- client/modules/core/components/Testimonials.jsx
+++ client/modules/core/components/Testimonials.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Time from 'react-time';
 
 class Testimonials extends React.Component {
   render () {
@@ -12,11 +13,25 @@ class Testimonials extends React.Component {
             <h1 className="section-title">Testimonials</h1>
           </header>
           <div className="section-content pure-grid testimonials-content">
-            {testimonials.map((t) => (
-              <div className="profile-picture pure-u-1" key={t._id}>
-                <img className="cameo pure-img" title="referrer" src={"/images/profiles/" + t.avatar} />
-              </div>
-            ))}
+            {testimonials.map((t) => {
+              return (
+                <section className="testimonial pure-u-1" key={t._id}>
+                  <header>
+                    <img className="testimonial-profile-picture cameo pure-img" title="referrer" src={"/images/profiles/" + t.avatar} />
+                    <p className="testimonial-data testimonial-name">{t.name}</p>
+                    <p className="testimonial-data">{t.title}</p>
+                  </header>
+                  <p className="testimonial-recommendation">
+                    <i className="testimonial-recommendation--open fa fa-quote-left" ></i>
+                    {t.recommendation}
+                    <i className="testimonial-recommendation--close fa fa-quote-right"></i>
+                  </p>
+                  <footer>
+                    <p className="testimonial-tagline"><Time value={t.at} format="DD/MM/YYYY" />, {t.relationship}</p>
+                  </footer>
+                </section>
+              )
+            })}
           </div>
         </div>
       </section>
